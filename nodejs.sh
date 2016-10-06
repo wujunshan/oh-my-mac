64584504bf06821e71272184d55b6e029b3a1453
guxiaobai <sikuan.gu@gmail.com>
2016-10-06 08:38:03 -0700

    nvm fish problem

--- nodejs.sh
+++ nodejs.sh
@@ -1,29 +1,12 @@
 #!/bin/bash
 
-
-# initialize
-export NVM_DIR=~/.nvm
-export NVM_NODEJS_ORG_MIRROR=http://npm.taobao.org/mirrors/node
-mkdir $NVM_DIR
-
-# nvm
-brew install nvm watchman
-[ -s "$(brew --prefix nvm)/nvm.sh" ] && . "$(brew --prefix nvm)/nvm.sh"
-
-# node
-nvm install node
-nvm use node
+# node 
+brew install node watchman
 
 # npm
 npm config set registry https://registry.npm.taobao.org
-# npm install -g react-native-cli
-# npm install -g ember-cli
-# npm install -g express-generator
-# npm install -g bower
+npm install -g react-native-cli
+npm install -g ember-cli
+npm install -g express-generator
+npm install -g bower
 
-# profile for fish
-echo '# rbenv' | tee -a ~/.config/fish/config.fish ~/.zshrc ~/.bashrc
-tee -a ~/.zshrc ~/.bashrc << EOD
-export NVM_DIR=~/.nvm
-[ -s "$(brew --prefix nvm)/nvm.sh" ] && . "$(brew --prefix nvm)/nvm.sh"
-EOD
